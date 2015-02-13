af87959b319c4734e6fd9244397a262505f7c778
Vladster1256 <vcmariorocks@gmail.com>
2015-02-13 13:38:54 -0700

    wowkid
    woot, maybe something idk

--- src/dimensions/view/DimensionsPanel.java
+++ src/dimensions/view/DimensionsPanel.java
@@ -2,6 +2,7 @@ package dimensions.view;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
 import dimensions.controller.DimensionController;
 
@@ -13,12 +14,16 @@ public class DimensionsPanel extends JPanel
 	private JLabel appLabel;
 	private SpringLayout baseLayout;
 	private JScrollPane tablePane;
+	private TableCellWrapRenderer myCellRenderer;
 	
 	public DimensionsPanel(DimensionController baseController)
 	{
 		this.baseController = baseController;
 		appLabel = new JLabel("look 2D data :D");
+		
+		myCellRenderer = new TableCellWrapRenderer();
 		dimensionTable = new JTable();
+		
 		baseLayout = new SpringLayout();
 		baseLayout.putConstraint(SpringLayout.NORTH, dimensionTable, 200, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, dimensionTable, 50, SpringLayout.WEST, this);
@@ -33,8 +38,15 @@ public class DimensionsPanel extends JPanel
 	{
 		String [] colHeader = {"Column 1", "Column 2"};
 		dimensionTable = new JTable(new DefaultTableModel(baseController.getMyFood(), colHeader));
+		for(int col = 0; col < dimensionTable.getColumnCount(); col++)
+		{
+			dimensionTable.getColumnModel().getColumn(col).setCellRenderer( myCellRenderer);
+		}
 		tablePane = new JScrollPane(dimensionTable);
+		
 	}
+	
+	
 	
 	private void setupPanel()
 	{
